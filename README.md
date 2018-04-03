43cb252d53c21ffcb6187e0eb90151205f0dfae4
Deva Sagar <deva.sagar@oracle.com>
2018-04-03 15:06:59 -0400

    fixed env var names

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # oci-objectstore-sync
 
+This step is under development. README will be available when it is ready for use.
+
--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ set_auth() {
  
   echo '[DEFAULT]' > "$CONFIG_FILE"
   echo "user=${WERCKER_OCI_OBJECTSTORE_SYNC_USER_OCID}" >> "$CONFIG_FILE"
-  echo "fingerprint=${WERCKER_OCI_OBJECTSTORE_SYNC_TENANCY_OCID}" >> "$CONFIG_FILE"
+  echo "tenancy=${WERCKER_OCI_OBJECTSTORE_SYNC_TENANCY_OCID}" >> "$CONFIG_FILE"
   echo "fingerprint=${WERCKER_OCI_OBJECTSTORE_SYNC_FINGERPRINT}" >> "$CONFIG_FILE"
   echo "region=${WERCKER_OCI_OBJECTSTORE_SYNC_REGION}" >> "$CONFIG_FILE"
   echo "key_file=${key_file}" >> "$CONFIG_FILE"
@@ -26,15 +26,21 @@ set_auth() {
 main() {
   set_auth
 
-  info 'starting s3 synchronisation'
+  #Python 3 has ascii as locale default which makes a library ("click") used by ocicli to fail.
+  #Explicitly set locale to UTF-8
+  export LANG=C.UTF-8
+  export LC_ALL=C.UTF-8
+  info 'starting OCI object store synchronisation with OCI version:'
+  $WERCKER_STEP_ROOT/oci --version
+
+  if [ ! -n "$WERCKER_OCI_OBJECTSTORE_SYNC_TENANCY_OCID" ]; then
+    fail 'missing or empty option tenancy_ocid, please check wercker.yml'
+  fi
 
   if [ ! -n "$WERCKER_OCI_OBJECTSTORE_SYNC_USER_OCID" ]; then
     fail 'missing or empty option user_ocid, please check wercker.yml'
   fi
 
-  if [ ! -n "$WERCKER_OCI_OBJECTSTORE_SYNC_TENANCY_OCID" ]; then
-    fail 'missing or empty option tenancy_ocid, please check wercker.yml'
-  fi
 
   if [ ! -n "$WERCKER_OCI_OBJECTSTORE_SYNC_FINGERPRINT" ]; then
     fail 'missing or empty option fingerprint, please check wercker.yml'
@@ -57,46 +63,41 @@ main() {
     fail 'missing or empty option namespace, please check wercker.yml'
   fi
 
-  if [ ! -n "$WERCKER_OCI_OBJECTSTORE_SYNC_FILE" ]; then
-    fail 'missing or empty option file, please check wercker.yml'
+  if [ ! -n "$WERCKER_OCI_OBJECTSTORE_SYNC_SOURCE_DIR" ]; then
+    fail 'missing or empty option source_dir, please check wercker.yml'
   fi
 
   if [ ! -n "$WERCKER_OCI_OBJECTSTORE_SYNC_OBJECT_NAME" ]; then
     fail 'missing or empty option object_name, please check wercker.yml'
   fi
 
-  if [[ ! -e $WERCKER_OCI_OBJECTSTORE_SYNC_FILE || ! -r $WERCKER_OCI_OBJECTSTORE_SYNC_FILE ]] ; then
-    fail 'specified file does not exist or is not readable'
+  if [[ ! -d $WERCKER_OCI_OBJECTSTORE_SYNC_SOURCE_DIR || -L $WERCKER_OCI_OBJECTSTORE_SYNC_SOURCE_DIR ]] ; then
+    fail 'specified source directory does not exist or is not readable'
   fi
 
-#  if [ ! -n "$WERCKER_S3SYNC_OPTS" ]; then
-#    export WERCKER_S3SYNC_OPTS="--acl-public"
-#  fi
+  if [ ! -n "$WERCKER_OCI_OBJECTSTORE_SYNC_OBJECT_PREFIX" ]; then
+    WERCKER_OCI_OBJECTSTORE_SYNC_OBJECT_PREFIX="$(basename $WERCKER_OCI_OBJECTSTORE_SYNC_SOURCE_DIR)/"
+  fi
 
-#  source_dir="$WERCKER_ROOT/$WERCKER_S3SYNC_SOURCE_DIR"
-#  if cd "$source_dir";
-#  then
-#      debug "changed directory $source_dir, content is: $(ls -l)"
-#  else
-#      fail "unable to change directory to $source_dir"
-#  fi
+  if [[ "$WERCKER_OCI_OBJECTSTORE_SYNC_OVERWRITE" == "true" || "$WERCKER_OCI_OBJECTSTORE_SYNC_OVERWRITE" == "TRUE" ]]; then
+    OVERWRITE_FLAG="--overwrite"
+  else
+    OVERWRITE_FLAG="--no-overwrite"
+  fi  
 
   set +e
-  local SYNC="oci --config-file $CONFIG_FILE os object put --namespace $WERCKER_OCI_OBJECTSTORE_SYNC_NAMESPACE --bucket-name WERCKER_OCI_OBJECTSTORE_SYNC_BUCKET_NAME --name $WERCKER_OBJECTSTORE_SYNC_OBJECT_NAME --file $WERCKER_OBJECTSTORE_SYNC_FILE"
+  local SYNC="$WERCKER_STEP_ROOT/oci --config-file $CONFIG_FILE os object bulk-upload $WERCKER_OCI_OBJECTSTORE_SYNC_OPTIONS $OVERWRITE_FLAG --namespace $WERCKER_OCI_OBJECTSTORE_SYNC_NAMESPACE --bucket-name $WERCKER_OCI_OBJECTSTORE_SYNC_BUCKET_NAME --src-dir $WERCKER_OCI_OBJECTSTORE_SYNC_SOURCE_DIR --object-prefix $WERCKER_OCI_OBJECTSTORE_SYNC_OBJECT_PREFIX"
 
-  #local SYNCPUT="oci --config-file ~/.oci/config os object put --namespace odx-pipelines --bucket-name grappler-testing --name devausingcli.txt --file $TODO_FILE_TO_PUT"
-  #local SYNCGET="oci --config-file $TODO_CONFIG_FILE os object get --namespace odx-pipelines --bucket-name grappler-testing --name tmp.json --file $TODO_OUTPUT_FILE"
-#"$WERCKER_STEP_ROOT/s3cmd sync $WERCKER_S3SYNC_OPTS $WERCKER_S3SYNC_DELETE_REMOVED --verbose ./ $WERCKER_S3SYNC_BUCKET_URL"
   debug "$SYNC"
-  echo "not running"
-  #local sync_output=$($SYNC)
+  echo "running"
+  local sync_output=$($SYNC)
 
   if [[ $? -ne 0 ]];then
       echo "$sync_output"
       fail 'oci os put failed';
   else
       echo "$sync_output"
-      success 'finished oci object store synchronisation';
+      success 'completed oci object store synchronisation';
   fi
   set -e
 }
